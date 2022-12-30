3c126190bce1c4efa64b42afe87df93691e4917a
Nadia <nadya-koshelkovskaya@yandex.ru>
2022-12-30 09:45:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+        int x=10513; // стоимость билета
+        int y=x/20; //  количество миль, начисляемые за покупку билета
+
+        System.out.println("Количество миль за покупку билета - " + y);
+    }
+}
